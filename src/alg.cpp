b5a3215e61d4f59cd9286d4a8a87d900857217e1
Denis Suchkov <99133137+PlagueDoctoh@users.noreply.github.com>
2022-04-02 01:09:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -21,8 +21,41 @@ int priority (char ch) {
 
 std::string infx2pstfx(std::string inf) {
   // добавьте код
-  
-  return std::string("");
+  Tstack <char, 100> stck;
+  std::string ps;
+  for (int i = 0; i < inf.size(); i++) {
+    if (priority(inf[i]) == -1) {
+      if (ps.empty() == False && priority(inf[i - 1]) != -1) {
+        stck.push_back(' ');
+      }
+      stck.push_back(inf[i]);
+    } else if (priority(inf[i]) == 0) || priority(inf[i]) > priority(stck.get()) || stck.isEmpty()) {
+            stck.push(inf[i]);
+        } else {
+            if ((priority(inf[i]) == 1)) {
+                while (stck.get() != '(') {
+                    ps.push_back(' ');
+                    ps.push_back(stck.get());
+                    stck.pop();
+                }
+                stck.pop();
+            } else {
+                while (priority(stck.get()) >= priority(inf[i]) {
+                    ps.push_back(' ');
+                    ps.push_back(stck.get());
+                    stck.pop();
+                }
+                stck.push(inf[i]);
+            }
+        }
+    }
+    while (!stck.isEmpty()) {
+        ps.push_back(' ');
+        ps.push_back(stck.get());
+        stck.pop();
+    }
+    return ps;
+}
 }
 
 int eval(std::string pref) {
