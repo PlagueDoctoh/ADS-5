ccfef12860485218da9796bf68516e49cfb4a3fb
Denis Suchkov <99133137+PlagueDoctoh@users.noreply.github.com>
2022-04-01 22:40:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,30 @@
 #include <map>
 #include "tstack.h"
 
+int priority (char ch) {
+  if (ch == '(') {
+    return 0;
+  }
+  if (ch == ')') {
+    return 1;
+  }
+  if (ch ==  '+' || ch == '-') {
+    return 2;
+  }
+  if (ch == '*' || ch == '/') {
+    return 3;
+  }
+  return -1;
+}
+
 std::string infx2pstfx(std::string inf) {
   // добавьте код
+  
   return std::string("");
 }
 
 int eval(std::string pref) {
   // добавьте код
+  
   return 0;
 }
