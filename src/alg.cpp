f950c9f97aeddb1017f3e2fa88f0afaec2aa3028
Denis Suchkov <99133137+PlagueDoctoh@users.noreply.github.com>
2022-04-02 02:32:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -20,7 +20,6 @@ int priority(char ch) {
 }
 
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
   Tstack <char, 100> stck;
   std::string ps;
   for (int i = 0; i < inf.size(); i++) {
@@ -60,7 +59,40 @@ std::string infx2pstfx(std::string inf) {
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  
-  return 0;
+  TStack <int, 100> stck1;
+  std::string temp;
+  int num = 0, mun = 0;
+  size_t beg = 0, fin = 0;
+  for (int i = 0; i < pref.size(); i++) {
+    if (pref[i] == ' ' || i == pref.size()-1) {
+      fin = i;
+      if (i == pref.size() - 1) 
+        fin++;
+      temp = pref.substr(beg, fin - beg);
+      beg = fin + 1;
+      bool num = true;
+      for (int i = 0; i < temp.size; i++)
+        if (temp[i] < '0' || temp[i] > '9') {
+          num = false;
+          break;
+        }
+    }
+    if (num = true) {
+      stck1.push(std::stoi(temp));
+    } else {
+      mun = stck1.get();
+      stck1.pop();
+      num = stck1.get();
+      stck1.pop();
+      if (temp == "+")
+        stck1.push(num + mun);
+      else if (temp == "-")
+        stck1.push(num - mun);
+      else if (temp == "*")
+        stck1.push(num * mun);
+      else if (temp == "/")
+        stck1.push(num / mun);
+    }
+  }
+  return stck1.get();
 }
